4da7a4eb57c4767d43cac5e591d2b30402f12844
Juan Namendi <jnamendi@buyawg.com>
2025-01-23 05:35:01 -0600

    remove secret from code

--- src/main/java/com/awg/wiredash_apis/configuration/DataSourceConfiguration.java
+++ src/main/java/com/awg/wiredash_apis/configuration/DataSourceConfiguration.java
@@ -13,10 +13,10 @@ public class DataSourceConfiguration {
    @Primary 
    public SQLServerDataSource getDataSource() {
     	SQLServerDataSource ds = new SQLServerDataSource();
-        ds.setServerName("orisqnmw76hu5iuz67xlpa3t5a-iyslpm74fxqehcusupnitvaezm.datawarehouse.fabric.microsoft.com"); // Replace with your server name
-        ds.setDatabaseName("LAKE_Dynamics365AWG_UAT"); // Replace with your database
-        ds.setUser("d10c86fd-e782-4b09-8701-9b22489eb573"); // Replace with your username
-        ds.setPassword("QSK8Q~1854MzspX7XQcxhBLuo.hZPH6IMrwYqa0O"); // Replace with your password
+        ds.setServerName("orisqnmw76hu5iuz67xlpa3t5a-iyslpm74fxqehcusupnitvaezm.datawarehouse.fabric.microsoft.com");
+        ds.setDatabaseName("LAKE_Dynamics365AWG_UAT");
+        ds.setUser("");
+        ds.setPassword("");
         ds.setAuthentication("ActiveDirectoryServicePrincipal");       
         ds.setTrustServerCertificate(true);
         return ds;
